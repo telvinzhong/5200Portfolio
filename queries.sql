485d1906fdb3aa53e5679798966bad62dbf6f068
Telvin Zhong <telvinzhong@gmail.com>
2020-10-28 16:09:18 -0500

    updated queries

---
+++ queries.sql
@@ -0,0 +1,36 @@
+-- Query One: Basic query
+-- Shows all Companies and their industries in a table.
+
+SELECT Company_Name, Industry FROM COMPANY;
+
+-- Query Two: Join three or more tables
+-- Shows the Institutions where people have studied for each company.
+
+Select CO.Company_Name, Ed.Institution
+FROM Work_Experience as We, Education as Ed, Person as Pe, Company as CO
+WHERE We.U_ID = Pe.U_ID and Ed.U_ID = Pe.U_ID and We.C_ID = CO.C_ID;
+
+
+-- Query Three: Contains subquery
+-- Finds the person with or people tied for the lowest gpa.
+
+SELECT Pe.first_name, Pe.last_name, Ed.GPA
+FROM Person as Pe, Education as Ed
+WHERE Pe.U_ID = Ed.U_ID AND GPA = (SELECT MIN(GPA) FROM EDUCATION);
+
+-- Query Four: group by...having clause
+-- Shows all jobs with more than three people in that role across all companies.
+
+SELECT Work_role, COUNT(*)
+FROM Work_Experience
+GROUP BY Work_role
+HAVING COUNT(*) > 3;
+
+-- Query Five: Complex search criterion.
+
+CREATE ASSERTION work_constraint
+CHECK(NOT EXISTS(
+    SELECT *
+    FROM WORK_EXPERIENCE
+    WHERE End_day < Start_day
+));
